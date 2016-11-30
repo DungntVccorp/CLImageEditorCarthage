9be9677ff3462c57a1e7c32fe1ece4a6085bda62
Nguyen Dung <dungnt@vivas.vn>
2016-11-30 11:17:50 +0700

    edit source

---
+++ CLImage/CLImage.h
@@ -0,0 +1,19 @@
+//
+//  CLImage.h
+//  CLImage
+//
+//  Created by Nguyen Dung on 11/30/16.
+//  Copyright © 2016 Nguyen Dung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CLImage.
+FOUNDATION_EXPORT double CLImageVersionNumber;
+
+//! Project version string for CLImage.
+FOUNDATION_EXPORT const unsigned char CLImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLImage/PublicHeader.h>
+
+
