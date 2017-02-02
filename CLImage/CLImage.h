4d97a8a640ded8026b01525901edad07f92684b4
Nguyen Dung <dungnt@vivas.vn>
2017-02-02 13:05:28 +0700

    edit header

--- CLImage/CLImage.h
+++ CLImage/CLImage.h
@@ -19,6 +19,37 @@ FOUNDATION_EXPORT const unsigned char CLImageVersionString[];
 #import <CLImage/CLImageEditorTheme.h>
 #import <CLImage/CLImageToolInfo.h>
 #import <CLImage/_CLImageEditorViewController.h>
+#import <CLImage/CLAdjustmentTool.h>
+#import <CLImage/CLBloomEffect.h>
+#import <CLImage/CLBlurTool.h>
+#import <CLImage/CLCircleView.h>
+#import <CLImage/CLClassList.h>
+#import <CLImage/CLClippingTool.h>
+#import <CLImage/CLColorPickerView.h>
+#import <CLImage/CLDrawTool.h>
+#import <CLImage/CLEffectBase.h>
+#import <CLImage/CLEffectTool.h>
+#import <CLImage/CLFilterBase.h>
+#import <CLImage/CLFilterTool.h>
+#import <CLImage/CLGloomEffect.h>
+#import <CLImage/CLHighlightShadowEffect.h>
+#import <CLImage/CLHueEffect.h>
+#import <CLImage/CLImageEditorTheme+Private.h>
+#import <CLImage/CLImageToolBase.h>
+#import <CLImage/CLImageToolInfo+Private.h>
+#import <CLImage/CLImageToolProtocol.h>
+#import <CLImage/CLImageToolSettings.h>
+#import <CLImage/CLPixellateEffect.h>
+#import <CLImage/CLPosterizeEffect.h>
+#import <CLImage/CLRotateTool.h>
+#import <CLImage/CLSplineInterpolator.h>
+#import <CLImage/CLSpotEffect.h>
+#import <CLImage/CLToneCurveTool.h>
+#import <CLImage/CLToolbarMenuItem.h>
+#import <CLImage/UIDevice+SystemVersion.h>
+#import <CLImage/UIImage+Utility.h>
+#import <CLImage/UIView+CLImageToolInfo.h>
+#import <CLImage/UIView+Frame.h>
 
 
 
